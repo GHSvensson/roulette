8fa7a2860503defe12c9df2b12c31578cbb146ff
GHSvensson <gustavthesvensson@gmail.com>
2023-09-21 16:06:47 +0200

    Added new functions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <main>
+    <div id="autoSpin">
+      <input v-model="autoSpinCounter">
+      <button @click="autoSpin">Auto Spin</button>
+    </div>
     <div>
       <Transition name="fade" mode="out-in">
         <p :key="displayNumber">{{displayNumber}}</p>
@@ -11,7 +15,7 @@
         <input v-model="evens" type="number" name="evens" id="evens">
         <button @click="evens=''">Clear</button>
       </div>
-      
+
       <div class="input-container">
         <label for="odds">Odds: {{ oddsMulti }}x</label>
         <span>{{ oddsWon ? '✅' : '❌' }}</span>
@@ -22,8 +26,9 @@
       <Transition name="fade" mode="out-in">
         <p :key="money">{{money}}:-</p>
       </Transition>
-    </div>
-    <button class="bet-button" @click="generateNumber">BET</button>
+      <button class="bet-button" @click="generateNumber">BET</button>
+    <input v-model="startingMoney">
+    <button @click="money=startingMoney">New Balance</button>
     <div id="results">
       <div>
         <TransitionGroup name="results">
@@ -32,7 +37,12 @@
       </div>
       <button @click="results=[]">Clear</button>
     </div>
-  
+   <GameOver v-if="showGameOverModal" @hideModal="restartGame"></GameOver>
+    </div>
+    
+   <div class="tracker">
+   {{tenStreak}}
+  </div>
   </main>
 </template>
 
@@ -40,6 +50,8 @@
 import { ref } from 'vue';
 import HistoryNumber from './components/HistoryNumber.vue';
 import { computed } from 'vue';
+import GameOver from './components/GameOver.vue';
+import { v4 as uuidv4 } from 'uuid';
 
 
 const displayNumber = ref(0)
@@ -49,6 +61,12 @@ const results = ref([])
 const money = ref(100000)
 const evensMulti = ref(1)
 const oddsMulti = ref(1)
+const startingMoney = ref()
+const showGameOverModal = ref(false)
+const consecutiveEvens = ref(0)
+const consecutiveOdds = ref(0)
+const tenStreak = ref(0)
+const autoSpinCounter = ref(0)
 
 const evensWon = computed(()=>{
   return (displayNumber.value % 2 === 0 && displayNumber.value !== 0)
@@ -64,7 +82,7 @@ function generateNumber(){
   displayNumber.value = Math.floor(Math.random() * 38);
   results.value.unshift({
    number: displayNumber.value,
-   id:Date.now()
+   id:uuidv4()
   })
   results.value = results.value.slice(0, 76)
 
@@ -74,6 +92,14 @@ function generateNumber(){
     money.value += evens.value * evensMulti.value
     oddsMulti.value *= 2
     evensMulti.value = 1
+
+    consecutiveEvens.value++;
+    consecutiveOdds.value = 0
+
+    if (consecutiveEvens.value === 10){
+      tenStreak.value++;
+      consecutiveEvens.value = 0;
+    }
   }
 
   if (oddsWon.value){
@@ -81,12 +107,40 @@ function generateNumber(){
     money.value += odds.value * oddsMulti.value
     evensMulti.value *= 2
     oddsMulti.value = 1
-  }
+
+    consecutiveOdds.value++;
+    consecutiveEvens.value = 0;
+
+    if (consecutiveOdds.value === 10){
+      tenStreak.value++;
+      consecutiveOdds.value = 0;
+  }}
     //multiplier applied to both in case of zero
   if (rolledZero.value){
     evensMulti.value *= 2
     oddsMulti.value *= 2
   }
+  if (money.value<=0){
+    showGameOverModal.value = true;
+  }
+}
+
+function autoSpin(){
+  for (let i = 0; i < autoSpinCounter.value; i++) {
+  generateNumber();
+}
+}
+
+function restartGame(){
+  showGameOverModal.value = false;
+   displayNumber.value = 0
+   odds.value = 0
+   evens.value = 0
+   results.value = []
+   money.value = 100000
+   evensMulti.value = 1
+   oddsMulti.value = 1
+   startingMoney.value = 0
 }
 
 </script>
@@ -106,6 +160,8 @@ input::-webkit-inner-spin-button {
 
 main{
   display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 5rem;
   height: 100%;
   place-content: center;
   max-width: 500px;
@@ -129,9 +185,22 @@ label{
   text-align: center;
 }
 
+#autoSpin{
+  
+}
+
+.tracker{
+  width: 10rem;
+  height: 10rem;
+  background-color: white;
+  font-size: 5rem;
+  text-align: center;
+}
+
 .bet-button{
  width: auto;
  font-size: 2rem;
+ margin-right: 2rem;
 }
 
 #results>div{
---
+++ src/components/GameOver.vue
@@ -0,0 +1,33 @@
+<template>
+<Teleport to="body">
+<div @click.self="$emit('hideModal')">
+    <p>Your balance is less than 0:-</p>
+</div>
+</Teleport>
+</template>
+
+<script setup>
+defineEmits(['hideModal'])
+</script>
+
+<style scoped>
+div{
+width: 100dvw;
+height: 100dvh;
+position: fixed;
+display: flex;
+top: 0;
+left: 0;
+}
+
+p{
+margin: auto;
+font-size: 5rem;
+background-color: black;
+padding: 2rem;
+border-radius: 3rem;
+
+}
+
+</style>
+
